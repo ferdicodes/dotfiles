5a4567bcd74137ae0be11df7ef1d77e6f1011b6e
Ferdi <ferdinand@sayhero.com>
2017-07-26 21:45:16 +0000

    Add a simple script to install my vim config

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/dotfiles/vim-config" ]; then
+    echo "Installing vim config..."
+    git clone --depth=1 https://github.com/ferdicodes/dotfiles "$HOME/.vimconf"
+
+    FILES=$HOME/.vimconf/vim-config/*
+    LOCATION=$HOME/.config/vim/
+
+    ln -s $HOME/.vimconf/.vimrc $HOME/.vimrc
+
+    for file in $FILES; do
+    	ln -s $file $LOCATION
+    done
+else
+    echo "Your vim-config is already installed."
+fi
